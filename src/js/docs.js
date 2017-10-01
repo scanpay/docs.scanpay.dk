456cbe48cc21537f92a993b79b8930c05e19b87c
Ulrik Moe <ulrik.moe@gmail.com>
2017-10-01 16:24:20 +0200

    Fix eslint fetch-indent. Use fetch for GA.js

--- src/js/docs.js
+++ src/js/docs.js
@@ -58,36 +58,36 @@ function search(query) {
         'fields=items(title,snippet,link)&q=';
 
     fetch(url + encodeURI(query))
-    .then(res => res.json())
-    .then((o) => {
-        if (!o.items) {
-            results.textContent = 'No search results.';
+        .then(res => res.json())
+        .then((o) => {
+            if (!o.items) {
+                results.textContent = 'No search results.';
+                spinner.remove();
+                return;
+            }
+
+            const list = document.createDocumentFragment();
+            for (let x = 0; x < o.items.length; x++) {
+                const li = document.createElement('li');
+                const h2 = document.createElement('h2');
+                const a = document.createElement('a');
+                h2.className = 'search--h2';
+
+                const item = o.items[x];
+                a.textContent = item.title;
+                a.href = item.link;
+                h2.appendChild(a);
+                li.appendChild(h2);
+                li.appendChild(document.createTextNode(item.snippet));
+                list.appendChild(li);
+            }
             spinner.remove();
-            return;
-        }
-
-        const list = document.createDocumentFragment();
-        for (let x = 0; x < o.items.length; x++) {
-            const li = document.createElement('li');
-            const h2 = document.createElement('h2');
-            const a = document.createElement('a');
-            h2.className = 'search--h2';
-
-            const item = o.items[x];
-            a.textContent = item.title;
-            a.href = item.link;
-            h2.appendChild(a);
-            li.appendChild(h2);
-            li.appendChild(document.createTextNode(item.snippet));
-            list.appendChild(li);
-        }
-        spinner.remove();
-        results.appendChild(list);
-    })
-    .catch(() => {
-        spinner.remove();
-        results.textContent = 'Error: Please try to reload this page';
-    });
+            results.appendChild(list);
+        })
+        .catch(() => {
+            spinner.remove();
+            results.textContent = 'Error: Please try to reload this page';
+        });
 }
 
 
@@ -147,14 +147,12 @@ function selectAll() {
             localStorage._ga = cid = (Math.random() +
                 '00000000000000000000').substring(2, 21) + time;
         }
-
-        let d = 'v=1&tid=UA-45595918-1&ds=web&cid=' + cid;
+        let url = '/_ga/collect?v=1&tid=UA-45595918-1&ds=web&cid=' +
+                    cid + '&z=' + time;
         for (const k in o) {
-            d += '&' + k + '=' + o[k];
+            url += '&' + k + '=' + o[k];
         }
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', '/_ga/collect?' + d + '&z=' + time);
-        xhr.send();
+        fetch(url);
     }
 
     _ga({
