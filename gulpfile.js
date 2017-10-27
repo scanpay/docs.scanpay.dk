4c47a42f95e0087b8109c1c69d16fe02c47e23ff
Ulrik Moe <ulrik.moe@gmail.com>
2017-10-27 14:23:41 +0200

    Use Node.Path Merge webserver and watch Use mo3.flatten and avoid manupulating env.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 /* @author ScanPay ApS */
 /* eslint-env node */
 const fs = require('fs');
+const Path = require('path');
 const gulp = require('gulp');
 const connect = require('gulp-connect');
 const lessjs = require('less');
 const hljs = require('highlight.js');
 const through = require('through2');
-const mo3place = require('mo3place')();
+const mo3 = require('mo3place')();
 const env = require('minimist')(process.argv.slice(2));
 env.currentYear = (new Date()).getFullYear();
 const index = JSON.parse(fs.readFileSync('src/index.json', 'utf8'));
@@ -45,36 +46,18 @@ for (let i = 0; i < index.length; i++) {
     files[o.path] = o;
 }
 
-function webserver() {
-    connect.server({
-        root: 'www',
-        livereload: true,
-        middleware: () => ([(req, res, next) => {
-            const path = req.url.split('?')[0];
-            const ext = path.slice((path.lastIndexOf('.') - 1 >>> 0) + 2);
-            if (!ext && path !== '/') {
-                req.url = path + '.html';
-            }
-            next();
-        }])
-    });
-}
-
 function html() {
     return gulp.src('src/*.html')
         .pipe(through.obj((file, enc, cb) => {
             // Get meta data from links{}
-            env.filename = file.path.substring(__dirname.length + 5);
-            if (files[env.filename]) {
-                for (const key in files[env.filename]) {
-                    env[key] = files[env.filename][key];
-                }
-            }
+            const filename = Path.basename(file.path);
+            const obj = mo3.flatten([env, files[filename]]);
+            obj.filename = filename;
 
-            // mo3place w. template.
-            file.contents = Buffer.from(mo3place.render(mo3place
+            // mo3 w. template.
+            file.contents = Buffer.from(mo3.render(mo3
                 .getStr('src/code/header.tpl.html') + file.contents.toString() +
-                    mo3place.getStr('src/code/footer.tpl.html'), env));
+                    mo3.getStr('src/code/footer.tpl.html'), obj));
             cb(null, file);
         }))
         .pipe(gulp.dest('www'))
@@ -93,8 +76,8 @@ function code() {
             } else {
                 str = hljs.highlight(ext, str, true).value;
             }
-            // Save highlighted code to mo3place's cache
-            mo3place.setCache(file.path.substring(__dirname.length + 1), str);
+            // Save highlighted code to mo3's cache
+            mo3.setCache(file.path.substring(__dirname.length + 1), str);
             file.contents = Buffer.from(str);
             cb(null, file);
         }))
@@ -104,8 +87,8 @@ function code() {
 function htmlCode() {
     return gulp.src('src/code/**/*.html')
         .pipe(through.obj((file, enc, cb) => {
-            const str = mo3place.render(file.contents.toString());
-            mo3place.setCache(file.path.substring(__dirname.length + 1), str);
+            const str = mo3.render(file.contents.toString());
+            mo3.setCache(file.path.substring(__dirname.length + 1), str);
             file.contents = Buffer.from(str);
             cb(null, file);
         }))
@@ -115,13 +98,12 @@ function htmlCode() {
 function assets() {
     return gulp.src(['src/assets/*.*', 'src/assets/font/*.*'])
         .pipe(through.obj((file, enc, cb) => {
-            const lastDot = file.path.lastIndexOf('.');
-            if (file.path.substring(lastDot) === '.less') {
+            if (Path.extname(file.path) === '.less') {
                 // Convert LESS to CSS.
                 lessjs.render(file.contents.toString(), null, (err, res) => {
                     if (err) { throw err; }
                     file.contents = Buffer.from(res.css);
-                    file.path = file.path.substring(0, lastDot) + '.css';
+                    file.path = file.path.slice(0, -4) + 'css';
                     cb(null, file);
                 });
             } else { cb(null, file); }
@@ -131,19 +113,28 @@ function assets() {
 }
 
 function images() {
-    return gulp.src('src/img/**')
-        .pipe(gulp.dest('www/img/'))
-        .pipe(connect.reload());
+    return gulp.src('src/img/**').pipe(gulp.dest('www/img/')).pipe(connect.reload());
 }
 
-
-function stalker() {
+function webserver() {
+    connect.server({
+        root: 'www',
+        livereload: true,
+        middleware: () => ([(req, res, next) => {
+            const path = req.url.split('?')[0];
+            const ext = path.slice((path.lastIndexOf('.') - 1 >>> 0) + 2);
+            if (!ext && path !== '/') {
+                req.url = path + '.html';
+            }
+            next();
+        }])
+    });
     gulp.watch(['src/*.html'], html);
     gulp.watch(['src/assets/**/*.*'], assets);
     gulp.watch(['src/img/**'], images);
     gulp.watch('src/code/**/*.*', gulp.series(code, htmlCode, html));
 }
 
-gulp.task('build', gulp.series(gulp.parallel(assets, images, code), htmlCode, html));
-gulp.task('serve', gulp.parallel(stalker, webserver));
+gulp.task('build', gulp.series(assets, images, code, htmlCode, html));
+gulp.task('serve', gulp.series(webserver));
 gulp.task('default', gulp.series('build', 'serve'));
